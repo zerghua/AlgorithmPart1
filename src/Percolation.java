e5212318a0e09c6f01b4308ab5701e9db32fa13d
zerghua <zerghua@gmail.com>
2017-01-25 15:08:19 -0500

    faster implementation.

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,9 +1,11 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.Stopwatch;
-import java.util.*;  //this is not allowed
 
 /*
+
+naive implementation, check each openedLastRow with each openedFirstRow for percolation, will incur o(n^2) for such check
+
 
  > java PercolationStats 200 10
  mean                     = 0.5961874999999999
@@ -17,20 +19,25 @@ stddev                   = 0.009043118930988348
 95% confidence interval  = 0.5918516876590411, 0.5943583123409589
 Time                     = 15.674
 
+faster implementation(100x faster)
+ > java PercolationStats 200 20
+mean                     = 0.59133375
+stddev                   = 0.0072390855912448406
+95% confidence interval  = 0.5903304639238128, 0.5923370360761872
+Time                     = 0.09
+
+
 0 1 2 3
 4 5 6 7
 8 9 10 11
 12 13 14 15
 
- */
 
 public class Percolation {
     private boolean[] openedSite;
     private WeightedQuickUnionUF site;
     private int numberOfOpenSites=0;
     private int size;
-    private ArrayList<Integer> openedFirstRow;
-    private ArrayList<Integer> openedLastRow;
 
     private boolean isValidSite(int row, int col){
         if(row <1 || row >size|| col <1 || col >size) return false;
@@ -47,9 +54,6 @@ public class Percolation {
         size=n;
         openedSite = new boolean[size*size];
         site = new WeightedQuickUnionUF(size*size);
-        openedFirstRow = new ArrayList();
-        openedLastRow = new ArrayList();
-
     }
 
     public void open(int row, int col)    // open site (row, col) if it is not open already
@@ -60,14 +64,11 @@ public class Percolation {
             openedSite[index] = true;
             numberOfOpenSites++;
 
-            // union with neighbours if they are open TODO, check is valid site and isOpen
+            // union with neighbours if they are open
             if(isValidSite(row-1, col) && isOpen(row-1, col)) site.union(index, getSiteIndex(row-1, col));
             if(isValidSite(row+1, col) && isOpen(row+1, col)) site.union(index, getSiteIndex(row+1, col));
             if(isValidSite(row, col+1) && isOpen(row, col+1)) site.union(index, getSiteIndex(row, col+1));
             if(isValidSite(row, col-1) && isOpen(row, col-1)) site.union(index, getSiteIndex(row, col-1));
-
-            if(index < size) openedFirstRow.add(index);
-            if(index >= size*(size-1)) openedLastRow.add(index);
         }
     }
 
@@ -83,15 +84,10 @@ public class Percolation {
         int index = getSiteIndex(row, col);
         // check if it's connected to top row
         if(isOpen(row, col)){ // itself should open
-            for(int item: openedFirstRow){
-                if(site.connected(item, index))return true;
-            }
-
-            /* scan last row
             for(int i=1;i<=size;i++){
                 if(isOpen(1, i) && site.connected(index, i-1)) return true; // top row should open and they should connect
             }
-            */
+
         }
         return false;
     }
@@ -104,14 +100,9 @@ public class Percolation {
     public boolean percolates()              // does the system percolate?
     {
         //check if any of site in last row is full
-        for(int item: openedLastRow){
-            if(isFull(size, 1 + item%size))return true;
-        }
-        /*
         for(int i=1;i<=size;i++){
             if(isFull(size, i)) return true;
         }
-        */
         return false;
     }
 
@@ -144,77 +135,105 @@ public class Percolation {
 
 }
 
-
-/*
-this solution has a bug, when a middle one is opened which connects top and bottom, but last row is not updated.
+ */
 
 
-private boolean[] isOpen;
-private boolean[] isFull;
-private boolean isPercolates;
-
-for each unopened cell:
-// 1. open(row, col)
-// open cell: set isOpen[index] = true;
-// union neighbours,  union(p, q) , at most 4* logN
-
-// if first row, set it's full.
-        isGroupFull= false;
-        if(index <= n) {
-        isFull[index] = true;
-        isGroupFull = true;
-        }
-
-// check with neighbours, if any of them isFull, set all of them(5) to be full.
-        if(isFull(left)) isGroupFull = true;
-        if(isFull(right)) isGroupFull = true;
-        if(isFull(up)) isGroupFull = true;
-        if(isFull(down)) isGroupFull = true;
-
-        if(isGroupFull) {
-        isFull[left] =true;
-        isFull[right] =true;
-        isFull[up] =true;
-        isFull[down] =true;
-
-        // if the index from any of the 5 are >= n(n-1) and they are full, set isPercolates=true;
-        if(left_index >= n*(n-1)) isPercolates=true;
-        if(right_index >= n*(n-1)) isPercolates=true;
-        if(up_index >= n*(n-1)) isPercolates=true;
-        if(down_index >= n*(n-1)) isPercolates=true;
-        }
-
-
-// 2. isPercolates()
-
-*/
-
-
-
-/*
+// smart implementation,
+// use sentinel variable top/bottom for faster check. this is effectively like have a list of elements.
+// running from above 1s to 0.038s for n=200
 public class Percolation {
+    private boolean[][] openedSite; //
+    private WeightedQuickUnionUF site;
+    private int numberOfOpenSites, size, top, bottom;
+
     public Percolation(int n)                // create n-by-n grid, with all sites blocked
+    {
+        if(n<=0) throw new IllegalArgumentException("n is less than 1");
+        size=n;
+        numberOfOpenSites=0;
+        openedSite = new boolean[size+1][size+1];
+        site = new WeightedQuickUnionUF(size*size+2);
+        top = 0;
+        bottom = size*size+1;
+    }
+
     public void open(int row, int col)    // open site (row, col) if it is not open already
+    {
+        if(!isValidSite(row, col)) throw new IndexOutOfBoundsException("row or col is out of bound, row="+row + " col="+col);
+        if(openedSite[row][col] == true) return;  //we don't do anything if it's opened already
+
+        int index = getSiteIndex(row, col);
+        openedSite[row][col] = true;
+        numberOfOpenSites++;
+
+        // key to faster implementation
+        if(row == 1) site.union(top, getSiteIndex(row, col));
+        if(row == size) site.union(bottom, getSiteIndex(row, col));
+
+        // union with neighbours if they are open
+        if(isValidSite(row-1, col) && isOpen(row-1, col)) site.union(index, getSiteIndex(row-1, col));
+        if(isValidSite(row+1, col) && isOpen(row+1, col)) site.union(index, getSiteIndex(row+1, col));
+        if(isValidSite(row, col+1) && isOpen(row, col+1)) site.union(index, getSiteIndex(row, col+1));
+        if(isValidSite(row, col-1) && isOpen(row, col-1)) site.union(index, getSiteIndex(row, col-1));
+
+    }
+
     public boolean isOpen(int row, int col)  // is site (row, col) open?
-    public boolean isFull(int row, int col)  // is site (row, col) full?  use connected(p,q) to check itself against first row o(n)
+    {
+        if(!isValidSite(row, col)) throw new IndexOutOfBoundsException("row or col is out of bound, row="+row + " col="+col);
+        return openedSite[row][col];
+    }
+
+    public boolean isFull(int row, int col)  // is site (row, col) full?
+    {
+        if(!isValidSite(row, col)) throw new IndexOutOfBoundsException("row or col is out of bound, row="+row + " col="+col);
+        return site.connected(getSiteIndex(row, col), top);
+    }
+
     public int numberOfOpenSites()       // number of open sites
-    public boolean percolates()              // does the system percolate? isFull on last row o(n^2)
+    {
+        return numberOfOpenSites;
+    }
+
+    public boolean percolates()              // does the system percolate?
+    {
+        return site.connected(top, bottom);
+    }
+
+    private double getThreshold(){
+        int n = size, total = n*n;
+        int[] blockedSites= new int[total];
+        for(int i=0;i<total;i++) blockedSites[i] = i;
+        StdRandom.shuffle(blockedSites);
+
+        for(int i=0;i<total;i++) {
+            int row = 1 + blockedSites[i] / n, col = 1 + blockedSites[i] % n;
+            open(row, col);
+            if(i>=n && percolates()){
+                return numberOfOpenSites*1.0/total;
+            }
+        }
+        return 0; //should never reach here
+    }
+
+    private boolean isValidSite(int row, int col){
+        if(row <1 || row >size|| col <1 || col >size) return false;
+        return true;
+    }
+
+    // index of cell is between [1, n*n], 0 and n*n + 1 are reserved as sentinels.
+    private int getSiteIndex(int row, int col){
+        return size * (row - 1) + col;
+    }
+
 
     public static void main(String[] args)   // test client (optional)
-
-
-    //solution 1, store openedFirstRow and opendedLastRow.
-    // isFull(), use connected(p, each_item_in_openedFirstRow)
-
-
-    // percolates(),  use connected(each_item_in_openedLastRow, each_item_in_openedFirstRow)
-    // call isFull() on each_item_in_openedLastRow.
-    // pruning, only check percolates when numberofOpenSites >=n ;
-
-
-
-
+    {
+        int n=200;
+        Stopwatch time = new Stopwatch();
+        Percolation p = new Percolation(n);
+        System.out.println(p.getThreshold());
+        System.out.println(String.format("%-25s= ","Time") + time.elapsedTime());
+    }
 
 }
-
-*/
