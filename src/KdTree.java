6d0e498dca9addd30a3bc9e199cc88d7b444d86f
Hua <zerghua@gmail.com>
2017-02-22 17:37:17 -0500

    some functions ready.

--- src/KdTree.java
+++ src/KdTree.java
@@ -76,7 +76,6 @@
  of points n, using the memory-cost model from lecture and Section 1.4 of the textbook.
  Count all memory that is used by your 2d-tree, including memory for the nodes, points, and rectangles.
 
-
  Give the expected running time in seconds (using tilde notation) to build a 2d-tree on n random points
  in the unit square. (Do not count the time to read in the points from standard input.)
 
@@ -91,38 +90,114 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.LinkedList;
-import java.util.TreeSet;
 public class KdTree {
+    private Node root;
+    private int size;
+
+    private static class Node {
+        public Point2D p;      // the point
+        public RectHV rect;    // the axis-aligned rectangle corresponding to this node
+        public Node lb;        // the left/bottom subtree
+        public Node rt;        // the right/top subtree
+        public boolean isVertical;
+
+        public Node(Point2D p, RectHV rect){
+            this.p = p;
+            this.rect = rect;
+            this.lb = null;
+            this.rt = null;
+        }
+    }
 
     // construct an empty set of points
     public KdTree(){
-
+        size = 0;
     }
 
     // is the set empty?
     public boolean isEmpty(){
-        return false;
+        return size() == 0;
     }
 
     // number of points in the set
     public int size(){
-        return 0;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p){
+        if ( p == null ) throw new java.lang.NullPointerException("point should not be null");
+        if( root == null) {
+            root = new Node(p, new RectHV(0,0,1,1));
+            size++;
+        }else{
+            insert(root, root, p, true);
+        }
+    }
+
+    private Node insert(Node cur, Node parent, Point2D p, boolean isVertical){ // parent is used to calculate rectHV
+        if(cur == null) {
+            size++;
+            double xmin = parent.rect.xmin();
+            double ymin = parent.rect.ymin();
+            double xmax = parent.rect.xmax();
+            double ymax = parent.rect.ymax();
+
+            if(isVertical){
+                if(p.x() < parent.p.x()) xmax = parent.p.x();
+                else xmin = parent.p.x();
+            }else{
+                if(p.y() < parent.p.y()) ymax = parent.p.y();
+                else ymin = parent.p.y();
+            }
+
+            return new Node(p, new RectHV(xmin, ymin, xmax, ymax));
+        }
+
+        if(p.equals(cur.p)) return cur; // discard repeat point
+
+        if(cur.isVertical){ // compare x when vertical
+            if(p.x() < cur.p.x()) cur.lb = insert(cur.lb, cur, p, !isVertical);
+            else cur.rt = insert(cur.rt, cur, p, !isVertical);
+        }else{  // compare y when horizontal
+            if(p.y() < cur.p.y()) cur.lb = insert(cur.lb, cur, p, !isVertical);
+            else cur.rt = insert(cur.rt, cur, p, !isVertical);
+        }
+        return cur;
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p){
-        return false;
+        if ( p == null ) throw new java.lang.NullPointerException("point should not be null");
+        return contains(p, root, true);
+    }
+
+    private boolean contains(Point2D p, Node cur, boolean isVertical){
+        if(cur == null) return false;
+        if(p.equals(cur.p)) return true;
+
+        if(isVertical){
+            if(p.x() < cur.p.x()) return contains(p, cur.lb, !isVertical);
+            else return contains(p, cur.rt, !isVertical);
+        }else{
+            if(p.y() < cur.p.y()) return contains(p, cur.lb, !isVertical);
+            else return contains(p, cur.rt, !isVertical);
+        }
     }
 
 
-    // draw all points to standard draw
+    // draw all of the points to standard draw in black
+    // and the subdivisions in red (for vertical splits) and blue (for horizontal splits)
+    // Use StdDraw.setPenColor(StdDraw.BLACK) and StdDraw.setPenRadius(0.01) before before drawing the points;
+    // use StdDraw.setPenColor(StdDraw.RED) or StdDraw.setPenColor(StdDraw.BLUE) and StdDraw.setPenRadius()
+    // before drawing the splitting lines.
     public void draw(){
+        // StdDraw.line(xmin, ymin, xmax, ymin);
+        // StdDraw.setPenColor(StdDraw.BLACK);
+        // StdDraw.setPenRadius(0.01);
 
     }
 
